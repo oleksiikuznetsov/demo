1d8a6cb9375f1affe92e1483dee34ba525b94ae2
alex <44065666+webhoo9ster@users.noreply.github.com>
2019-02-07 12:04:43 +0200

    add del for out task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,35 @@
     // create variables for do our task
 const gulp        = require('gulp'),
-      sass        = require('gulp-sass'),
-      browserSync = require('browser-sync');
+      sass        = require('gulp-sass');
 
     // create task's
 
     // task sass
-gulp.task('sass', function() {
+    gulp.task('sass', function() {
     return gulp.src('app/sass/index.sass', gulp.series('sass'))
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}))
 });
-
-    // task browser-sync
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        },
-        notify: false
-    });
-});
     // task watch
 gulp.task('watch', function() {
-    gulp.watch('app/sass/index.sass', gulp.series('sass'))
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch('app/js*.js').on('change', browserSync.reload)
 });
 
-gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+gulp.task('default', gulp.parallel('watch', 'sass', 'browser-sync'));
+
+
+gulp.task('build', function() {
+    let buildCss = gulp.src('app/css/index.css')
+    .pipe(gulp.dest('dist/css'));
+
+    let buildFonts = gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+
+    let buildJs = gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'));
+
+    let buildHtml = gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'));
+});
