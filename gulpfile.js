00be23ac3400f61aeeb0cd5178b13e12a42cf80f
alex <44065666+webhoo9ster@users.noreply.github.com>
2019-02-07 04:52:11 +0200

    install browser sync to our moduls

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,27 @@
-const gulp    = require('gulp'),
-      sass    = require('gulp-sass');
+    // create variables for do our task
+const gulp        = require('gulp'),
+      sass        = require('gulp-sass'),
+      browserSync = require('browser-sync');
+
+    // create task's
+
+    // task sass
+gulp.task('sass', function() {
+    return gulp.src('app/sass/index.sass', gulp.series('sass'))
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+});
+
+    // task browser-sync
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+    // task watch
+gulp.task('watch', function() {
+    gulp.watch('app/sass/index.sass', gulp.series('sass'))
+});
